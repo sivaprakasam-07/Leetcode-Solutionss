3ea916cfb9ad883829c81ab37297706f0d7d7b77
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-01-14 22:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.25%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> temp;
+        int j=-1;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                temp.push_back(nums[i]);
+            if(nums[i]==0){
+                j=i;
+                break;
             }
         }
-        int nz=temp.size();
-        for(int i=0;i<nz;i++){
-            nums[i]=temp[i];
+        if(j==-1){
+            return ;
         }
-
-        for(int i=nz;i<nums.size();i++){
-            nums[i]=0;
+        for(int i=j+1;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
-        // return nums;
+        return ;
     }
 };
