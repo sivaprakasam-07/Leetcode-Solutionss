fac8e5dd10cb50b938f4d45e51cc9bccd525cdc3
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-01-18 21:50:48 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (76.17%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,42 +1,18 @@
-// class Solution {
-// public:
-//     vector<int> findDisappearedNumbers(vector<int>& nums) {
-//         int n=nums.size();
-//         vector<int> miss;
-//         vector<bool> seen(n+1, false);
-
-//         for(int x:nums){
-//             seen[x]=true;
-//         }
-//         for(int i=1;i<=n;i++){
-//             if(!seen[i]){
-//                 miss.push_back(i);
-//             }
-//         }
-//         return miss;
-//     }
-// };
-
-
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans;
+        int n=nums.size();
+        vector<int> miss;
+        vector<bool> seen(n+1, false);
 
-        for (int i = 0; i < n; i++) {
-            int idx = abs(nums[i]) - 1;
-            if (nums[idx] > 0) {
-                nums[idx] = -nums[idx];
+        for(int x:nums){
+            seen[x]=true;
+        }
+        for(int i=1;i<=n;i++){
+            if(!seen[i]){
+                miss.push_back(i);
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                ans.push_back(i + 1);
-            }
-        }
-
-        return ans;
+        return miss;
     }
 };
