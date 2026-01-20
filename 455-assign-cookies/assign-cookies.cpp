bd6a000a85b596e7be6c78a3b5a163f3b236884f
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-01-20 22:33:54 +0530

    Time: 17 ms (8.20%) | Memory: 44.9 MB (45.30%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,36 +1,19 @@
-// class Solution {
-// public:
-//     int findContentChildren(vector<int>& g, vector<int>& s) {
-//         int count=0;
-//         for(int i=0;i<g.size();i++){
-//             for(int j=0;j<s.size();j++){
-//                 if(s[j]>=g[i]){
-//                     count++;
-//                     break;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// };
-
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0,j=0,flag=0;
 
-        int i = 0, j = 0, count = 0;
-
-        while (i < g.size() && j < s.size()) {
-            if (s[j] >= g[i]) {
-                count++;
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i]){
+                flag++;
                 i++;
                 j++;
-            } else {
+            }else{
                 j++;
             }
         }
-        return count;
+        return flag;
     }
 };
