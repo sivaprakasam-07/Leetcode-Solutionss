eee59ff2855efe29bc5483c3c814696816670224
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-01-26 22:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (99.47%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -6,15 +6,5 @@ public:
         reverse(nums.begin(),nums.end());
         reverse(nums.begin(),nums.begin()+k);
         reverse(nums.begin()+k,nums.end());
-    //     vector<int>temp(k);
-    //     for(int i=0;i<k;i++){
-    //         temp[i]=nums[n-k+i];
-    //     }
-    //     for(int i=n-1;i>=k;i--){
-    //         nums[i]=nums[i-k];
-    //     }
-    //     for(int i=0;i<k;i++){
-    //         nums[i]=temp[i];
-    //     }
     }
 };
