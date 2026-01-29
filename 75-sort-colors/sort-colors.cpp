802d874139213e43e0a95395025fa743042f3778
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-01-29 22:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (77.38%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-            int count0=0,count1=0,count2=0;
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]==0){
-                    count0++;
-                }else if(nums[i]==1){
-                    count1++;
-                }else{
-                    count2++;
-                }
+        int n=nums.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high]);
+                high--;
             }
-            for(int i=0;i<count0;i++){
-                nums[i]=0;
-            }
-            for(int i=count0;i<count0+count1;i++){
-                nums[i]=1;
-            }
-            for(int i=count0+count1;i<nums.size();i++){
-                nums[i]=2;
-            }
-            return ;
+        }
     }
 };
