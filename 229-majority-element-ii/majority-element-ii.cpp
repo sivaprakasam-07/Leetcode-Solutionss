25142ac3a0943bd2370a1d0ddc6abafc7a34f71e
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-02-11 23:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (63.74%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,20 +1,3 @@
-// class Solution {
-// public:
-//     vector<int> majorityElement(vector<int>& nums) {
-//         int n=nums.size();
-//         unordered_map<int,int> freq;
-//         vector<int> ans;
-//         for(int x:nums){
-//             freq[x]++;
-//         }
-//         for(auto it:freq){
-//             if(it.second>n/3){
-//                 ans.push_back(it.first);
-//             }
-//         }
-//         return ans;
-//     }
-// };
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
