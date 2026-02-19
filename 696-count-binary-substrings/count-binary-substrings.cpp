ba917f0e550a49cb69ff3c4f3603774c3031ac9f
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-02-19 21:52:33 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (82.66%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -5,6 +5,7 @@ public:
         int currGroup=1;
         int ans=0;
         for(int i=1;i<s.length();i++){
+            
             if(s[i]==s[i-1]){
                 currGroup++;
             }else{
