6b51457ad9f7e32f4ca562535adb8da1087e3dcf
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-02-23 23:03:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (22.72%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxProduct=nums[0];
-        for(int i=0;i<nums.size();i++){
-            int product=1;
-            for(int j=i;j<nums.size();j++){
-                product*=nums[j];
-                maxProduct=max(product,maxProduct);
+        int currentMax=nums[0];
+        int currentMin=nums[0];
+        int globalMax=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<0){
+                swap(currentMax,currentMin);
             }
+            currentMax=max(nums[i],currentMax*nums[i]);
+            currentMin=min(nums[i],currentMin*nums[i]);
+            globalMax=max(globalMax,currentMax);
         }
-        return maxProduct;
+        return globalMax;
     }
 };
