8d3c76b1afba8cbcd398a8a44a7830b3593c0d23
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-03-01 23:01:12 +0530

    Time: 3 ms (41.68%) | Memory: 16.3 MB (97.26%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -4,9 +4,7 @@ public:
         int maxDigit=n[0]-'0';
         for(int i=1;i<n.length();i++){
             int digit=n[i]-'0';
-            if(maxDigit<digit){
-                maxDigit=digit;
-            }
+            maxDigit=max(maxDigit,digit);
         }
         return maxDigit;
     }
