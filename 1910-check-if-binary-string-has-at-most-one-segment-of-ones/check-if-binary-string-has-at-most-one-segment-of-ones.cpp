3221e5ed3f3d2303a31f367f6d3f8a89a123086d
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-03-06 23:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.93%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int segment=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='1' &&(i==0 || s[i-1]=='0')){
-                segment++;
-            }
-        }if(segment>1){
-            return false;
-        }else{
-            return true;
-        }
+        return s.find("01")==string::npos;
     }
 };
