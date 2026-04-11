127bb142e79af405ddddf6d7c7d2794702337cfd
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-04-11 22:23:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (93.83%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string cleaned = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            if (isalnum(s[i])) {
-                cleaned += tolower(s[i]);
-                }
-        }
-
-        int n = cleaned.length();
-        for (int i = 0; i < n / 2; i++) {
-            int j = n - 1 - i;
-            if (cleaned[i] != cleaned[j]) {
+        int start=0;
+        int end=s.size()-1;
+        while(start<=end){
+            if(!isalnum(s[start])){
+                start++;
+                continue;
+            }
+            if(!isalnum(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[start])!=tolower(s[end])){
                 return false;
+            }else{
+                start++;
+                end--;
             }
         }
-
         return true;
     }
 };
