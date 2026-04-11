56ce380bacb1bb4d5cc9cfcf23e8545445cbc8c1
SIVAPRAKASAM T <tamilmanisivaprakasam5@gmail.com>
2026-04-11 22:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (37.54%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int temp = x;
-        long long rev = 0;
-
-        while (x > 0) {
-            int digit = x % 10;
-            rev = (rev * 10) + digit;
-            x = x / 10;
+        int temp=x;
+        long long rev=0;
+        while(x>0){
+            int digit=x%10;
+            rev=(rev*10)+digit;
+            x=x/10;
         }
-
-        return rev == temp;
+        return rev==temp;
     }
 };
